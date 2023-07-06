470f1116c1b02303e484aee08ab738b8491d9a46
shinyquagsire23 <mtinc2@gmail.com>
2023-07-05 22:49:20 -0700

    More notes

--- build.sh
+++ build.sh
@@ -1,18 +1,21 @@
-
 #!/bin/zsh
 PWD=$(pwd)
 
-#xcrun mig \
-#    -arch arm64 \
-#    -DIOKIT \
-#    -I./include \
-#    ../xnu/osfmk/device/device.defs
+# Be sure to follow https://gist.github.com/shinyquagsire23/3c68aecd872cc7ac21c28e950245dbd2
 
-cd libusb
+# Monado build config
+#cmake .. -DXRT_ENABLE_GPL=1 -DXRT_BUILD_DRIVER_EUROC=0 -DXRT_BUILD_DRIVER_NS=0 -DXRT_BUILD_DRIVER_PSVR=0 -DXRT_HAVE_OPENCV=0 -DXRT_HAVE_XCB=0 -DXRT_HAVE_XLIB=0 -DXRT_HAVE_XRANDR=0 -DXRT_HAVE_SDL2=0  -DXRT_HAVE_VT=0 -DXRT_FEATURE_WINDOW_PEEK=0 -DXRT_BUILD_DRIVER_QWERTY=0
+
+pushd libusb
 #./bootstrap.sh
 make
-cd ..
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    exit $retVal
+fi
+popd
 
+# Build, and if it errors then abort
 cmake -B build -D CMAKE_BUILD_TYPE=RelWithDebInfo -D BUILD_TESTING=YES -G Ninja -S .
 ninja -C build
 retVal=$?
@@ -20,6 +23,7 @@ if [ $retVal -ne 0 ]; then
     exit $retVal
 fi
 
+# Compile all the shaders
 mkdir -p shaders && cp openxr_src/shaders/* shaders/ && cd $PWD && \
 glslc --target-env=vulkan1.2 $PWD/shaders/Basic.vert -std=450core -O -o $PWD/shaders/Basic.vert.spv && \
 glslc --target-env=vulkan1.2 $PWD/shaders/Cube.frag -std=450core -O -o $PWD/shaders/Cube.frag.spv && \
@@ -27,23 +31,10 @@ glslc --target-env=vulkan1.2 $PWD/shaders/Grid.frag -std=450core -O -o $PWD/shad
 
 cp build/libXRGyroControls.dylib libXRGyroControls.dylib
 
-# Copy to CoreSimulator
+# Remove old sim stuff
 rm -rf /Applications/Xcode-beta.app/Contents/Developer/Platforms/XROS.platform/Library/Developer/CoreSimulator/Profiles/UserInterface/XRGyroControls.simdeviceui
-#cp -r XRGyroControls.simdeviceui /Applications/Xcode-beta.app/Contents/Developer/Platforms/XROS.platform/Library/Developer/CoreSimulator/Profiles/UserInterface/XRGyroControls.simdeviceui
 
-# Weird lunarG Vulkan junk
-#maxamillion@Maxs-MacBook-Pro lib % mv libvulkan.dylib libvulkan.dylib_
-#maxamillion@Maxs-MacBook-Pro lib % mv libvulkan.1.dylib libvulkan.1.dylib_
-#maxamillion@Maxs-MacBook-Pro lib % mv libvulkan.1.3.250.dylib libvulkan.1.3.250.dylib_
-#maxamillion@Maxs-MacBook-Pro lib % cp libMoltenVK.dylib libvulkan.dylib
-#maxamillion@Maxs-MacBook-Pro lib % cp libMoltenVK.dylib libvulkan.1.dylib
-#maxamillion@Maxs-MacBook-Pro lib % cp libMoltenVK.dylib libvulkan.1.3.250.dylib
-
-
-# Monado build config
 # otool -L is your friend, make sure there's nothing weird
-#cmake .. -DXRT_ENABLE_GPL=1 -DXRT_BUILD_DRIVER_EUROC=0 -DXRT_BUILD_DRIVER_NS=0 -DXRT_BUILD_DRIVER_PSVR=0 -DXRT_HAVE_OPENCV=0 -DXRT_HAVE_XCB=0 -DXRT_HAVE_XLIB=0 -DXRT_HAVE_XRANDR=0 -DXRT_HAVE_SDL2=0  -DXRT_HAVE_VT=0 -DXRT_FEATURE_WINDOW_PEEK=0 -DXRT_BUILD_DRIVER_QWERTY=0
-
 function fixup_dependency ()
 {
     which_dylib=$1
@@ -54,6 +45,8 @@ function fixup_dependency ()
     install_name_tool -change @rpath/$vtool_dst $(pwd)/$vtool_dst $which_dylib
     install_name_tool -change $vtool_src $(pwd)/$vtool_dst $which_dylib
 
+    # Every framework has to be changed to remove "Versions/A/".
+    # You can also swap out frameworks for interposing dylibs here.
     install_name_tool -change /System/Library/Frameworks/CoreFoundation.framework/Versions/A/CoreFoundation /System/Library/Frameworks/CoreFoundation.framework/CoreFoundation $vtool_dst
     install_name_tool -change /System/Library/Frameworks/Security.framework/Versions/A/Security /System/Library/Frameworks/Security.framework/Security $vtool_dst
     install_name_tool -change /System/Library/Frameworks/IOKit.framework/Versions/A/IOKit /System/Library/Frameworks/IOKit.framework/IOKit $vtool_dst
@@ -70,6 +63,7 @@ function fixup_dependency ()
     codesign -s - $vtool_dst --force --deep --verbose
 }
 
+# *slow chanting* hacks, hacks, HACKS **HACKS**
 fixup_dependency libXRGyroControls.dylib /opt/homebrew/lib/libopenxr_loader.dylib
 fixup_dependency libXRGyroControls.dylib /Users/maxamillion/workspace/monado/build/src/xrt/targets/openxr/libopenxr_monado.dylib
 fixup_dependency libXRGyroControls.dylib $VULKAN_SDK/lib/libvulkan.1.dylib
@@ -77,30 +71,32 @@ fixup_dependency libXRGyroControls.dylib $VULKAN_SDK/../MoltenVK/dylib/iOS/libMo
 fixup_dependency libXRGyroControls.dylib /opt/homebrew/opt/glfw/lib/libglfw.3.dylib
 fixup_dependency libXRGyroControls.dylib /opt/homebrew/opt/libusb/lib/libusb-1.0.0.dylib
 
+#
+# libopenxr_monado.dylib fixups
+#
 #fixup_dependency libopenxr_monado.dylib /opt/homebrew/opt/hidapi/lib/libhidapi.0.dylib
 fixup_dependency libopenxr_monado.dylib /opt/homebrew/opt/libusb/lib/libusb-1.0.0.dylib
-
-#cp libusb/libusb/.libs/libusb-1.0.0.dylib libusb-1.0.0.dylib
 fixup_dependency libopenxr_monado.dylib libusb/libusb/.libs/libusb-1.0.0.dylib
-
 fixup_dependency libopenxr_monado.dylib /opt/homebrew/opt/x264/lib/libx264.164.dylib
 fixup_dependency libopenxr_monado.dylib $VULKAN_SDK/lib/libvulkan.1.dylib
 fixup_dependency libopenxr_monado.dylib $VULKAN_SDK/../MoltenVK/dylib/iOS/libMoltenVK.dylib
 fixup_dependency libopenxr_monado.dylib /opt/homebrew/opt/cjson/lib/libcjson.1.dylib
 fixup_dependency libopenxr_monado.dylib /opt/homebrew/opt/jpeg-turbo/lib/libjpeg.8.dylib
 
+# Pulled from iPhone X recovery ramdisk, some patches were done in a hex editor.
 vtool -remove-build-version macos -output  IOUSBLib_ios_hax.dylib IOUSBLib_ios_hax.dylib
 vtool -remove-build-version ios -output  IOUSBLib_ios_hax.dylib IOUSBLib_ios_hax.dylib
 vtool -set-build-version xrossim 1.0 1.0 -tool ld 902.11 -output IOUSBLib_ios_hax.dylib IOUSBLib_ios_hax.dylib
+codesign -s - IOUSBLib_ios_hax.dylib --force --deep --verbose
 
+# Fixup libusb bc we don't actually use the homebrew one
 vtool -remove-build-version macos -output libusb-1.0.0.dylib libusb-1.0.0.dylib 
 vtool -set-build-version xrossim 1.0 1.0 -tool ld 902.11 -output libusb-1.0.0.dylib libusb-1.0.0.dylib 
+codesign -s - libusb-1.0.0.dylib --force --deep --verbose
 
 #vtool -remove-build-version ios -output  IOUSBLib_ios_macos.dylib IOUSBLib_ios_macos.dylib
 #vtool -set-build-version macos 14.0 14.0 -tool ld 902.8 -output IOUSBLib_ios_macos.dylib IOUSBLib_ios_macos.dylib
 #codesign -s - IOUSBLib_ios_macos.dylib --force --deep --verbose
-
-codesign -s - IOUSBLib_ios_hax.dylib --force --deep --verbose
 
 cp libMoltenVK_iossim.dylib libMoltenVK.dylib
 fixup_dependency libMoltenVK.dylib libMoltenVK.dylib
@@ -109,10 +105,8 @@ fixup_dependency libMoltenVK.dylib libMoltenVK.dylib
 #vtool -remove-build-version macos -output $vtool_dst $vtool_src
 #vtool -set-build-version xrossim 1.0 1.0 -tool ld 902.11 -output $vtool_dst $vtool_dst
 
+# Sign everything just in case (it complains anyway)
 codesign -s - libopenxr_loader.dylib --force --deep --verbose
 codesign -s - libopenxr_monado.dylib --force --deep --verbose
 codesign -s - libvulkan.1.dylib --force --deep --verbose
-codesign -s - libusb-1.0.0.dylib --force --deep --verbose
-
-# Sign just in case (it complains anyway)
 codesign -s - libXRGyroControls.dylib --force --deep --verbose
