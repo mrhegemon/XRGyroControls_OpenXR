6823feea0506953e42323d322f8d6902f1d449b6
shinyquagsire23 <mtinc2@gmail.com>
2023-07-23 22:36:22 -0700

    Explicitly use xcode-beta vtool and install_name_tool

--- build.sh
+++ build.sh
@@ -53,35 +53,40 @@ cp build/libSim2OpenXR.dylib libSim2OpenXR.dylib
 # Remove old sim stuff
 rm -rf ${XCODE_BETA_PATH}/Contents/Developer/Platforms/XROS.platform/Library/Developer/CoreSimulator/Profiles/UserInterface/XRGyroControls.simdeviceui
 
+INSTALL_NAME_TOOL=${XCODE_BETA_PATH}/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/install_name_tool
+VTOOL=${XCODE_BETA_PATH}/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/vtool
+
 # otool -L is your friend, make sure there's nothing weird
 function fixup_dependency ()
 {
     which_dylib=$1
     vtool_src=$2
     vtool_dst=$(basename $2)
-    vtool -remove-build-version macos -output $vtool_dst $vtool_src
-    vtool -set-build-version 12 1.0 1.0 -tool ld 902.11 -output $vtool_dst $vtool_dst
-    install_name_tool -change @rpath/$vtool_dst $(pwd)/$vtool_dst $which_dylib
-    install_name_tool -change $vtool_src $(pwd)/$vtool_dst $which_dylib
+    cp $vtool_src $vtool_dst
 
     # Every framework has to be changed to remove "Versions/A/".
     # You can also swap out frameworks for interposing dylibs here.
-    install_name_tool -change /System/Library/Frameworks/CoreFoundation.framework/Versions/A/CoreFoundation /System/Library/Frameworks/CoreFoundation.framework/CoreFoundation $vtool_dst
-    install_name_tool -change /System/Library/Frameworks/Security.framework/Versions/A/Security /System/Library/Frameworks/Security.framework/Security $vtool_dst
-    install_name_tool -change /System/Library/Frameworks/IOKit.framework/Versions/A/IOKit /System/Library/Frameworks/IOKit.framework/IOKit $vtool_dst
+    $INSTALL_NAME_TOOL -change /System/Library/Frameworks/CoreFoundation.framework/Versions/A/CoreFoundation /System/Library/Frameworks/CoreFoundation.framework/CoreFoundation $vtool_dst
+    $INSTALL_NAME_TOOL -change /System/Library/Frameworks/Security.framework/Versions/A/Security /System/Library/Frameworks/Security.framework/Security $vtool_dst
+    $INSTALL_NAME_TOOL -change /System/Library/Frameworks/IOKit.framework/Versions/A/IOKit /System/Library/Frameworks/IOKit.framework/IOKit $vtool_dst
     #install_name_tool -change /System/Library/Frameworks/IOKit.framework/Versions/A/IOKit $(pwd)/IOKit_arm64.dylib $vtool_dst
 
-    install_name_tool -change /System/Library/Frameworks/Metal.framework/Versions/A/Metal /System/Library/Frameworks/Metal.framework/Metal $vtool_dst
-    install_name_tool -change /System/Library/Frameworks/IOSurface.framework/Versions/A/IOSurface /System/Library/Frameworks/IOSurface.framework/IOSurface $vtool_dst
-    install_name_tool -change /System/Library/Frameworks/AppKit.framework/Versions/C/AppKit /System/Library/Frameworks/AppKit.framework/AppKit $vtool_dst
-    install_name_tool -change /System/Library/Frameworks/QuartzCore.framework/Versions/A/QuartzCore /System/Library/Frameworks/QuartzCore.framework/QuartzCore $vtool_dst
-    install_name_tool -change /System/Library/Frameworks/CoreGraphics.framework/Versions/A/CoreGraphics /System/Library/Frameworks/CoreGraphics.framework/CoreGraphics $vtool_dst
-    install_name_tool -change /System/Library/Frameworks/Foundation.framework/Versions/C/Foundation /System/Library/Frameworks/Foundation.framework/Foundation $vtool_dst
-    install_name_tool -change /System/Library/PrivateFrameworks/SoftLinking.framework/Versions/A/SoftLinking /System/Library/PrivateFrameworks/SoftLinking.framework/SoftLinking $vtool_dst
-    install_name_tool -change /System/Library/Frameworks/VideoToolbox.framework/Versions/A/VideoToolbox /System/Library/Frameworks/VideoToolbox.framework/VideoToolbox $vtool_dst
-    install_name_tool -change /System/Library/Frameworks/CoreServices.framework/Versions/A/CoreServices /System/Library/Frameworks/CoreServices.framework/CoreServices $vtool_dst
-    install_name_tool -change /System/Library/Frameworks/CoreMedia.framework/Versions/A/CoreMedia /System/Library/Frameworks/CoreMedia.framework/CoreMedia $vtool_dst
-    install_name_tool -change /System/Library/Frameworks/CoreVideo.framework/Versions/A/CoreVideo /System/Library/Frameworks/CoreVideo.framework/CoreVideo $vtool_dst
+    $INSTALL_NAME_TOOL -change /System/Library/Frameworks/Metal.framework/Versions/A/Metal /System/Library/Frameworks/Metal.framework/Metal $vtool_dst
+    $INSTALL_NAME_TOOL -change /System/Library/Frameworks/IOSurface.framework/Versions/A/IOSurface /System/Library/Frameworks/IOSurface.framework/IOSurface $vtool_dst
+    $INSTALL_NAME_TOOL -change /System/Library/Frameworks/AppKit.framework/Versions/C/AppKit /System/Library/Frameworks/AppKit.framework/AppKit $vtool_dst
+    $INSTALL_NAME_TOOL -change /System/Library/Frameworks/QuartzCore.framework/Versions/A/QuartzCore /System/Library/Frameworks/QuartzCore.framework/QuartzCore $vtool_dst
+    $INSTALL_NAME_TOOL -change /System/Library/Frameworks/CoreGraphics.framework/Versions/A/CoreGraphics /System/Library/Frameworks/CoreGraphics.framework/CoreGraphics $vtool_dst
+    $INSTALL_NAME_TOOL -change /System/Library/Frameworks/Foundation.framework/Versions/C/Foundation /System/Library/Frameworks/Foundation.framework/Foundation $vtool_dst
+    $INSTALL_NAME_TOOL -change /System/Library/PrivateFrameworks/SoftLinking.framework/Versions/A/SoftLinking /System/Library/PrivateFrameworks/SoftLinking.framework/SoftLinking $vtool_dst
+    $INSTALL_NAME_TOOL -change /System/Library/Frameworks/VideoToolbox.framework/Versions/A/VideoToolbox /System/Library/Frameworks/VideoToolbox.framework/VideoToolbox $vtool_dst
+    $INSTALL_NAME_TOOL -change /System/Library/Frameworks/CoreServices.framework/Versions/A/CoreServices /System/Library/Frameworks/CoreServices.framework/CoreServices $vtool_dst
+    $INSTALL_NAME_TOOL -change /System/Library/Frameworks/CoreMedia.framework/Versions/A/CoreMedia /System/Library/Frameworks/CoreMedia.framework/CoreMedia $vtool_dst
+    $INSTALL_NAME_TOOL -change /System/Library/Frameworks/CoreVideo.framework/Versions/A/CoreVideo /System/Library/Frameworks/CoreVideo.framework/CoreVideo $vtool_dst
+
+    $VTOOL -remove-build-version macos -output $vtool_dst $vtool_dst
+    $VTOOL -set-build-version 12 1.0 1.0 -tool ld 902.11 -output $vtool_dst $vtool_dst
+    $INSTALL_NAME_TOOL -change @rpath/$vtool_dst $(pwd)/$vtool_dst $which_dylib
+    $INSTALL_NAME_TOOL -change $vtool_src $(pwd)/$vtool_dst $which_dylib
 
     codesign -s - $vtool_dst --force --deep --verbose
 }
@@ -107,26 +112,26 @@ fixup_dependency libopenxr_monado.dylib /opt/homebrew/opt/cjson/lib/libcjson.1.d
 fixup_dependency libopenxr_monado.dylib /opt/homebrew/opt/jpeg-turbo/lib/libjpeg.8.dylib
 
 # Pulled from iPhone X recovery ramdisk, some patches were done in a hex editor.
-vtool -remove-build-version macos -output  IOUSBLib_ios_hax.dylib IOUSBLib_ios_hax.dylib
-vtool -remove-build-version ios -output  IOUSBLib_ios_hax.dylib IOUSBLib_ios_hax.dylib
-vtool -set-build-version 12 1.0 1.0 -tool ld 902.11 -output IOUSBLib_ios_hax.dylib IOUSBLib_ios_hax.dylib
+$VTOOL -remove-build-version macos -output  IOUSBLib_ios_hax.dylib IOUSBLib_ios_hax.dylib
+$VTOOL -remove-build-version ios -output  IOUSBLib_ios_hax.dylib IOUSBLib_ios_hax.dylib
+$VTOOL -set-build-version 12 1.0 1.0 -tool ld 902.11 -output IOUSBLib_ios_hax.dylib IOUSBLib_ios_hax.dylib
 codesign -s - IOUSBLib_ios_hax.dylib --force --deep --verbose
 
 # Fixup libusb bc we don't actually use the homebrew one
-vtool -remove-build-version macos -output libusb-1.0.0.dylib libusb-1.0.0.dylib 
-vtool -set-build-version 12 1.0 1.0 -tool ld 902.11 -output libusb-1.0.0.dylib libusb-1.0.0.dylib 
+$VTOOL -remove-build-version macos -output libusb-1.0.0.dylib libusb-1.0.0.dylib 
+$VTOOL -set-build-version 12 1.0 1.0 -tool ld 902.11 -output libusb-1.0.0.dylib libusb-1.0.0.dylib 
 codesign -s - libusb-1.0.0.dylib --force --deep --verbose
 
-#vtool -remove-build-version ios -output  IOUSBLib_ios_macos.dylib IOUSBLib_ios_macos.dylib
-#vtool -set-build-version macos 14.0 14.0 -tool ld 902.8 -output IOUSBLib_ios_macos.dylib IOUSBLib_ios_macos.dylib
+#$VTOOL -remove-build-version ios -output  IOUSBLib_ios_macos.dylib IOUSBLib_ios_macos.dylib
+#$VTOOL -set-build-version macos 14.0 14.0 -tool ld 902.8 -output IOUSBLib_ios_macos.dylib IOUSBLib_ios_macos.dylib
 #codesign -s - IOUSBLib_ios_macos.dylib --force --deep --verbose
 
 cp libMoltenVK_iossim.dylib libMoltenVK.dylib
 fixup_dependency libMoltenVK.dylib libMoltenVK.dylib
 #vtool_src=libvulkan.1.dylib
 #vtool_dst=libvulkan.1.dylib
-#vtool -remove-build-version macos -output $vtool_dst $vtool_src
-#vtool -set-build-version 12 1.0 1.0 -tool ld 902.11 -output $vtool_dst $vtool_dst
+#$VTOOL -remove-build-version macos -output $vtool_dst $vtool_src
+#$VTOOL -set-build-version 12 1.0 1.0 -tool ld 902.11 -output $vtool_dst $vtool_dst
 
 # Sign everything just in case (it complains anyway)
 codesign -s - libopenxr_loader.dylib --force --deep --verbose
@@ -145,7 +150,7 @@ gsed "s|REPLACE_ME|$PWD|g" openxr_monado-dev.json.template > openxr_monado-dev.j
 #
 
 # Couldn't figure out how to do this with CMake, but everything gets dynamically linked so it doesn't particularly matter and we can fixup the rpaths
-install_name_tool -change @rpath/libSimulatorKit.dylib  @rpath/SimulatorKit.framework/Versions/A/SimulatorKit build/libXRGyroControls.dylib
+$INSTALL_NAME_TOOL -change @rpath/libSimulatorKit.dylib  @rpath/SimulatorKit.framework/Versions/A/SimulatorKit build/libXRGyroControls.dylib
 mkdir -p XRGyroControls.simdeviceui/Contents/MacOS/
 cp build/libXRGyroControls.dylib XRGyroControls.simdeviceui/Contents/MacOS/XRGyroControls
 
